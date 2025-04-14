6b35273e9aec4006f8cc70c1e5417ab5e9e80f6e
MahdiProgman <mahdihabibkah78@gmail.com>
2025-04-14 20:05:45 +0330

    made header responsive

--- scripts/global.js
+++ scripts/global.js
@@ -1,5 +1,9 @@
 const greetingText = document.getElementById('greeting-text');
 const greetingIcon = document.getElementById('greeting-icon');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+const drawerBox = document.querySelector('header .drawer-menu .drawer');
+const overlay = document.querySelector('body > .overlay');
 const time = new Date();
 
 if (time.getHours() >= 6 && time.getHours() <= 12) {
@@ -12,3 +16,17 @@ if (time.getHours() >= 6 && time.getHours() <= 12) {
     greetingText.innerText = 'شب بخیر';
     greetingIcon.src = '/assets/icons/night.png';
 }
+
+menuBtn.addEventListener('click', ()=> {
+    if(!drawerBox.classList.contains('on')){
+        drawerBox.classList.add('on');
+        overlay.classList.remove('hidden');
+    }
+});
+
+closeBtn.addEventListener('click', ()=> {
+    if(drawerBox.classList.contains('on')){
+        drawerBox.classList.remove('on');
+        overlay.classList.add('hidden');
+    }
+})

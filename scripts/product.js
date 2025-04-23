e693003986cb5280231aa1a8a932c6109b92d39b
MahdiProgman <mahdihabibkah78@gmail.com>
2025-04-23 10:40:08 +0330

    created description section in product page

--- scripts/product.js
+++ scripts/product.js
@@ -2,6 +2,20 @@ const imagesBtn = document.querySelectorAll('.images-list > .image-btn');
 const imageViewer = document.querySelector('.image-viewer');
 const closeImageBtn = document.querySelector('.image-viewer .close-image-btn');
 const imageViwerImage = document.querySelector('.image-viewer img');
+const showMoreBtn = document.querySelector('.description-and-specifications .description .show-more-btn');
+const textOfDescription = document.querySelector('.description-and-specifications .description p');
+
+showMoreBtn.addEventListener('click', () => {
+    if(!showMoreBtn.classList.contains('on')){
+        showMoreBtn.classList.add('on');
+        textOfDescription.style.maxHeight = '1000px';
+        showMoreBtn.children[0].innerText = 'مشاهده کمتر';
+    } else {
+        showMoreBtn.classList.remove('on');
+        textOfDescription.style.maxHeight = '80px';
+        showMoreBtn.children[0].innerText = 'مشاهده بیشتر';
+    }
+});
 
 imagesBtn.forEach(imageBtn => {
     imageBtn.addEventListener('click', ()=> {

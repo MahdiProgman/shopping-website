ea1707b5d10b5cecb74f1802ec22a611b0fc5819
MahdiProgman <mahdihabibkah78@gmail.com>
2025-05-01 15:38:01 +0330

    created a box for write comment in product page

--- scripts/product.js
+++ scripts/product.js
@@ -4,6 +4,11 @@ const closeImageBtn = document.querySelector('.image-viewer .close-image-btn');
 const imageViwerImage = document.querySelector('.image-viewer img');
 const showMoreBtn = document.querySelector('.description-and-specifications .description .show-more-btn');
 const textOfDescription = document.querySelector('.description-and-specifications .description p');
+const writeCommentBtn = document.querySelector('.comments-section .rate-and-submit-comment .write-comment-btn');
+const submitCommentBox = document.querySelector('.submit-comment-box');
+const closeSubmitCommentBox = document.querySelector('.submit-comment-box .header .close-box');
+const addNewPositivePointBtn = document.querySelector('.submit-comment-box .positive-points .input button');
+const addNewNegetivePointBtn = document.querySelector('.submit-comment-box .negetive-points .input button');
 
 showMoreBtn.addEventListener('click', () => {
     if(!showMoreBtn.classList.contains('on')){
@@ -26,4 +31,71 @@ imagesBtn.forEach(imageBtn => {
             overlay.classList.add('hidden');
         }, { once : true });
     });
+});
+
+writeCommentBtn.addEventListener('click', ()=> {
+    submitCommentBox.classList.add('on');
+    overlay.classList.remove('hidden');
+
+    overlay.addEventListener('click', ()=> {
+        submitCommentBox.classList.remove('on');
+        overlay.classList.add('hidden');
+    });
+});
+
+closeSubmitCommentBox.addEventListener('click', ()=> {
+    submitCommentBox.classList.remove('on');
+    overlay.classList.add('hidden');
+});
+
+addNewPositivePointBtn.addEventListener('click', ()=> {
+    if(addNewPositivePointBtn.parentElement.children[0].value.length > 3) {
+        const newPositivePoint = document.createElement('li')
+        const liContent = `
+            <div class="text">
+                <img src="/assets/icons/plus-green.png" />
+                <span>${addNewPositivePointBtn.parentElement.children[0].value}</span>
+            </div>
+            <button class="delete-btn">
+                <img src="/assets/icons/trash.png" />
+            </button>
+        `;
+        
+        addNewPositivePointBtn.parentElement.children[0].value = "";
+
+        newPositivePoint.innerHTML = liContent;
+
+        addNewPositivePointBtn.parentElement.parentElement.children[1].appendChild(newPositivePoint);
+
+        newPositivePoint.children[1].addEventListener('click', ()=> {
+            addNewPositivePointBtn.parentElement.parentElement.children[1].removeChild(newPositivePoint);
+        })
+
+    }
+});
+
+addNewNegetivePointBtn.addEventListener('click', ()=> {
+    if(addNewNegetivePointBtn.parentElement.children[0].value.length > 3) {
+        const newNegetivePoint = document.createElement('li')
+        const liContent = `
+            <div class="text">
+                <img src="/assets/icons/minus-red.png" />
+                <span>${addNewNegetivePointBtn.parentElement.children[0].value}</span>
+            </div>
+            <button class="delete-btn">
+                <img src="/assets/icons/trash.png" />
+            </button>
+        `;
+        
+        addNewNegetivePointBtn.parentElement.children[0].value = "";
+
+        newNegetivePoint.innerHTML = liContent;
+
+        addNewNegetivePointBtn.parentElement.parentElement.children[1].appendChild(newNegetivePoint);
+
+        newNegetivePoint.children[1].addEventListener('click', ()=> {
+            addNewNegetivePointBtn.parentElement.parentElement.children[1].removeChild(newNegetivePoint);
+        })
+
+    }
 });

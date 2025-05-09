98945cdfc084b287b8bf7df8b24563a8cab6f54e
MahdiProgman <mahdihabibkah78@gmail.com>
2025-05-09 17:27:28 +0330

    added a feature to user repository to return user with password or not

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -3,23 +3,45 @@ const UserModel = require('./../sequelize/models/user.model');
 const User = UserModel(dataBase);
 
 module.exports = new (class {
-    async findByEmail(email) {
+    async findByEmail(email, returnWithPassword = false) {
         const userFound = await User.findOne({
             where: {
                 email: email
             }
         });
 
-        return userFound;
+        return userFound ? returnWithPassword ? {
+            id: userFound.id,
+            email: userFound.email,
+            first_name: userFound.first_name,
+            last_name: userFound.last_name,
+            password: userFound.password
+        } : {
+            id: userFound.id,
+            email: userFound.email,
+            first_name: userFound.first_name,
+            last_name: userFound.last_name,
+        } : null;
     }
 
-    async findById(id) {
+    async findById(id, returnWithPassword = false) {
         const userFound = await User.findByPk(id);
 
-        return userFound;
+        return userFound ? returnWithPassword ? {
+            id: userFound.id,
+            email: userFound.email,
+            first_name: userFound.first_name,
+            last_name: userFound.last_name,
+            password: userFound.password
+        } : {
+            id: userFound.id,
+            email: userFound.email,
+            first_name: userFound.first_name,
+            last_name: userFound.last_name,
+        } : null; 
     }
 
-    async createUser(email, first_name, last_name, password) {
+    async createUser(email, first_name, last_name, password, returnWithPassword = false) {
         const newUser = await User.create({ 
             email: email,
             first_name: first_name,
@@ -27,11 +49,17 @@ module.exports = new (class {
             password: password
         });
 
-        return {
+        return returnWithPassword ? {
             id: newUser.id,
             email: newUser.email,
             first_name: newUser.first_name,
-            last_name: newUser.last_name
+            last_name: newUser.last_name,
+            password: newUser.password
+        } : {
+            id: newUser.id,
+            email: newUser.email,
+            first_name: newUser.first_name,
+            last_name: newUser.last_name,
         };
     }
 })();

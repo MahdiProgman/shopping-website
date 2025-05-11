57620eefce36a0ab06f5b69210a39862048f089e
MahdiProgman <mahdihabibkah78@gmail.com>
2025-05-11 14:29:46 +0330

    used req.login_state and req.user to say to front-end the user is logined or not

--- src/controllers/pages.controller.js
+++ src/controllers/pages.controller.js
@@ -1,31 +1,67 @@
+const _ = require('lodash');
+
 const getHomePage = (req, res) => {
   res.render("index", {
-    isLoggedInNow: req.flash('isLoggedInNow')[0] ?? false
+    isLoggedInNow: req.flash('isLoggedInNow')[0] ?? false,
+    login_state: req.login_state,
+    user: req.user ? {
+      first_name: req.user.first_name
+    } : null
   });
 };
 
 const getProductPage = (req, res) => {
-  res.render("product");
+  res.render("product", {
+    login_state: req.login_state,
+    user: req.user ? {
+      first_name: req.user.first_name
+    } : null
+  });
 }
 
 const getProductsPage = (req, res) => {
-  res.render("products");
+  res.render("products", {
+    login_state: req.login_state,
+    user: req.user ? {
+      first_name: req.user.first_name
+    } : null
+  });
 };
 
 const getCartPage = (req, res) => {
-  res.render("cart");
+  res.render("cart", {
+    login_state: req.login_state,
+    user: req.user ? {
+      first_name: req.user.first_name
+    } : null
+  });
 };
 
 const getFavoritesPage = (req, res) => {
-  res.render("favorites");
+  res.render("favorites", {
+    login_state: req.login_state,
+    user: req.user ? {
+      first_name: req.user.first_name
+    } : null
+  });
 };
 
 const getAboutUsPage = (req, res) => {
-  res.render("about-us");
+  res.render("about-us", {
+    login_state: req.login_state,
+    user: req.user ? {
+      first_name: req.user.first_name
+    } : null
+  });
 };
 
 const getSupportPage = (req, res) => {
-  res.render("support");
+  res.render("support", {
+    login_state: req.login_state,
+    user: req.user ? {
+      first_name: req.user.first_name
+    } : null
+  });
 };
 
 module.exports = {

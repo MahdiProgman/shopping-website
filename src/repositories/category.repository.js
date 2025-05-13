13d5e321d4e9465f8efd807b373c6ef25f19cdb6
MahdiProgman <mahdihabibkah78@gmail.com>
2025-05-13 09:55:35 +0330

    category repository was created

---
+++ src/repositories/category.repository.js
@@ -0,0 +1,19 @@
+const { dataBase } = require('../core/db');
+const CategoryModel = require('../sequelize/models/category.model');
+const Category = CategoryModel(dataBase);
+
+module.exports = new (class {
+    async findAllCategories() {
+        const categoriesFound = await Category.findAll();
+
+        if(categoriesFound.length == 0) return null;
+
+        return categoriesFound.map(category => ({
+            name: category.name,
+            name_fa: category.name_fa,
+            short_explanation: category.short_explanation,
+            boxColor: category.boxColor,
+            image: category.image
+        }));
+    }
+})();

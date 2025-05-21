4df50a35d519e965df4dd7f4ea391b0e2828fd27
MahdiProgman <mahdihabibkah78@gmail.com>
2025-05-21 19:47:29 +0330

    implemented the login page

---
+++ scripts/login.js
@@ -0,0 +1,11 @@
+const showPasswordBtn = document.getElementById('show-password-btn');
+
+showPasswordBtn.addEventListener('click', ()=> {
+    if(showPasswordBtn.parentElement.children[1].type == 'password'){
+        showPasswordBtn.parentElement.children[1].type = 'text'
+        showPasswordBtn.children[0].src = '/assets/icons/closed-eye.png';
+    } else {
+        showPasswordBtn.parentElement.children[1].type = 'password';
+        showPasswordBtn.children[0].src = '/assets/icons/eye.png';
+    }
+});

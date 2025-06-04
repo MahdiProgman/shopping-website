b8bd611c170412d28374d4ed0f31dea2cd103c83
MahdiProgman <mahdihabibkah78@gmail.com>
2025-06-04 21:51:20 +0330

    used change user password route in change info page

--- public/scripts/change-info.js
+++ public/scripts/change-info.js
@@ -1,4 +1,7 @@
 const showPasswordBtns = document.querySelectorAll('.show-password-btn');
+const changePasswordForm = document.getElementById('change-password-form');
+const newPasswordInput = document.getElementById('new_password');
+const repeatNewPasswordInput = document.getElementById('repeat_new_password');
 const notyf = new Notyf({
   duration: 3000,
   ripple: true,
@@ -23,3 +26,16 @@ showPasswordBtns.forEach(showPasswordBtn => {
 });
 
 if(isUserUpdated) notyf.success('اطلاعات با موفقیت بروزرسانی شد');
+
+changePasswordForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (newPasswordInput.value !== repeatNewPasswordInput.value) {
+    notyf.error('رمز عبور جدید با تکرار آن مطابقت ندارد');
+  } else {
+    changePasswordForm.submit();
+  }
+});
+
+if(isPasswordNotMatch) notyf.error('رمز عبور وارد شده با رمز عبور فعلی شما مطابقت ندارد');
+else if(isNewPasswordMatch) notyf.error('نمی توانید رمز عبور فعلی خود را به عنوان رمز عبور جدید انتخاب کنید');
+else if(isNewPasswordSet) notyf.success('رمز عبور جدید با موفقیت تنظیم شد');

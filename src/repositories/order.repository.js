f125296077f61978b7c751f66230bf86ff7b3d92
MahdiProgman <mahdihabibkah78@gmail.com>
2025-06-04 15:40:44 +0330

    created a method for create a order with items in order repository

--- src/repositories/order.repository.js
+++ src/repositories/order.repository.js
@@ -16,4 +16,22 @@ module.exports = new (class {
 
     return count;
   }
+
+  async createOrderWithItems(user_id, productIds, totalPrice, status, status_color) {
+    const countOfOrdersInDB = await this.Order.count();
+    const newOrder = await this.Order.create({
+      order_code: `swo-${countOfOrdersInDB + 1}`,
+      status: status,
+      status_color: status_color,
+      total_price: totalPrice,
+      user_id: user_id
+    });
+
+    const orderItems = productIds.map(product_id => ({
+      order_id: newOrder.id,
+      product_id: product_id
+    }));
+
+    await this.OrderItem.bulkCreate(orderItems);
+  }
 })();
